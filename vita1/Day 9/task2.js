46fd624de118caeedbd442a4421dac56b0e0da8f
ashwinwaikar <ashwinwaikar@gmail.com>
2020-05-26 22:56:22 +0530

    Update task2.js

--- vita1/Day 9/task2.js
+++ vita1/Day 9/task2.js
@@ -1,7 +1,9 @@
 // write a self invoking function whose job is to return square of a number
 console.log("this is task 2");
 
-(function () {
-  var a = 5;
+var x=(function (a) {
+//   var a = 5;
   return document.write(a * a);
-})();
+})(5);
+
+document.getElementById("demo").innerHTML="Square of Number = "+ x;
